9c304ea4a131053ce7b21a5b7224cd34cbe109da
igallion <82122432+igallion@users.noreply.github.com>
2025-04-10 14:03:19 -0400

    Adds rate limit audit logging to Vault

--- ubuntu/terraform/audit.tf
+++ ubuntu/terraform/audit.tf
@@ -3,5 +3,16 @@ resource "vault_audit" "vault_audit_file" {
 
   options = {
     file_path = "/tmp/vault_audit.txt"
+
   }
+}
+
+resource "vault_generic_endpoint" "rate_limit_audit_logging" {
+  path = "sys/quotas/config"
+
+  data_json = jsonencode({
+    enable_rate_limit_audit_logging = true
+  })
+
+  depends_on = [vault_audit.vault_audit_file]
 }
